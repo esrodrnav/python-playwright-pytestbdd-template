352c57af135c53e75811015fcd36c079a835fad5
Estefania <esrodrnav@gmail.com>
2025-09-02 22:55:22 +0200

    feature: uncomment steps

--- tests/features/conftest.py
+++ tests/features/conftest.py
@@ -1,6 +1,9 @@
 from pytest_bdd import given, when, then, scenario
+from tests.pages.base_page import BasePage
 
 
 @given("the user is on the login page")
-def navigate_to_login_page(page):
-    pass
+def navigate_to_login_page(page, get_env_datas):
+    base_page = BasePage(page, get_env_datas)
+    base_page.navigate_to_base_url()
+    
